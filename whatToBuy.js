d1db7bb12f52e4641d0c0e2094d4a1dd112b8020
Arnold Kgaladi <arnoldtiisetso3@gmail.com>
2022-05-26 14:45:24 +0200

    Simnandi

--- whatToBuy.js
+++ whatToBuy.js
@@ -1,9 +1,26 @@
-
-// this function compares the price of a muffin & a cupcake
 function whatToBuy(cupcake_price, muffin_price){
-	
+	if (cupcake_price > muffin_price) {
+		return "Buy a muffin";
+	}else{
+		return "Buy a cupcake";
+	}
+}
+
+const muffinPriceElem = document.querySelector('.muffin_price');
+const cupcakePriceElem = document.querySelector('.cupcake_price');
+const CheckBtnElem = document.querySelector('.button');
+const messageElem = document.querySelector('.message')
+
+CheckBtnElem.addEventListener('click', whatToBuyTwo);
+// this function compares the price of a muffin & a cupcake
+function whatToBuyTwo(){
+	var m_price = muffinPriceElem.value;
+	var c_price = cupcakePriceElem.value;
 	// Buy a muffin
-
-	// But a cupcake
-
+	if (c_price > m_price) {
+		return messageElem.innerHTML = ("Buy a muffin");
+	}// But a cupcake
+	else{
+		return messageElem.innerHTML = ("Buy a cupcake");
+	}
 }
